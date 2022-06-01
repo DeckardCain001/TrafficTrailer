6fd1b1c7dcf8201f0a20b95b82d3e03bcc749f87
Adam <67346725+DeckardCain001@users.noreply.github.com>
2022-06-01 17:50:24 -0400

    Update fxmanifest.lua (fix double-quote)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5' 
 
 author 'Deckard Cain <Deckard Cain#7751>'
-description 'Traffic Trailer script for RedneckMod"
+description 'Traffic Trailer script for RedneckMod'
 version '1.0.1'
 
 -- What to run
